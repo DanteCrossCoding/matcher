3b6abb5e3d60f6c43a00e48dd1791cbf6682fe63
Sean Oyler <sean.oyler@gmail.com>
2021-01-12 21:37:37 -0800

    Adds express

---
+++ api/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/test', (req, res) => {
+  res.send("Backend connected!");
+});
+
+const port = process.env.PORT || 9000;
+
+app.listen(port);
+
+console.log("Server started listening on port " + port);
