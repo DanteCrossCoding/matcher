65c47f5365960d6e4a09eb814bf34758bd3a6ff2
Sean Oyler <sean.oyler@gmail.com>
2021-01-12 18:20:31 -0800

    Adds test router /testAPI

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   }
 
   callAPI() {
-    fetch("http://localhost:9000/")
+    fetch("http://localhost:9000/testAPI")
       .then((res) => res.text())
       .then((res) => this.setState({ apiResponse: res }));
   }
