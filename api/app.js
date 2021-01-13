ba5bb2a4c188464b3e2c19db5b8115ecf60f0323
Sean Oyler <sean.oyler@gmail.com>
2021-01-12 18:22:10 -0800

    Adds testAPI route

--- api/app.js
+++ api/app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 const cors = require("cors");
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
+const testAPIRouter = require('./routes/testAPI');
 const app = express();
 
 // view engine setup
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/testAPI', testAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
