797358394b1bb73d54b869ee517437f5e86685a6
Sean Oyler <xrysen@gmail.com>
2021-01-13 22:51:29 -0800

    Adds postgres database

--- api/build/index.js
+++ api/build/index.js
@@ -1,9 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+require('dotenv').config();
+const pg = require('pg-promise')();
+const db = pg(`postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/matcher`);
 const app = express();
 app.get('/test', (req, res) => {
     res.send("Backend connected!");
+});
+app.get('/', (req, res) => {
+    db.any(`SELECT * FROM users`)
+        .then((data) => {
+        res.send(data);
+    });
 });
 const port = process.env.PORT || 9000;
 app.listen(port);
--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,19 @@
 import express = require("express");
+require('dotenv').config();
+const pg = require('pg-promise')();
+const db = pg(`postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/matcher`);
 
 const app = express();
 
 app.get('/test', (req: any, res: any) => {
   res.send("Backend connected!");
+});
+
+app.get('/', (req: any, res: any) => {
+  db.any(`SELECT * FROM users`)
+  .then((data: any) => {
+    res.send(data);
+  });
 });
 
 const port = process.env.PORT || 9000;
