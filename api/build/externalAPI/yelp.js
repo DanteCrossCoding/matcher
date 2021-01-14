70ada603d1f32a3695d0a9abda7b0998a77eafad
Sean Oyler <xrysen@gmail.com>
2021-01-14 15:53:23 -0800

    Adds shuffleArray algorithm

--- api/build/externalAPI/yelp.js
+++ api/build/externalAPI/yelp.js
@@ -3,19 +3,54 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRestaurant = void 0;
+exports.getRestaurantsWithFilter = exports.getRestaurants = void 0;
 const axios_1 = __importDefault(require("axios"));
-require('dotenv').config();
-const getRestaurant = (location) => {
-    return axios_1.default.get(`https://api.yelp.com/v3/businesses/search?location=${location}&categories=restaurants&limit=10`, {
-        headers: { 'Authorization': `Bearer ${process.env.API_KEY_YELP}` }
+require("dotenv").config();
+const getRestaurants = (location) => {
+    return axios_1.default
+        .get(`https://api.yelp.com/v3/businesses/search?location=${location}&categories=restaurants`, {
+        headers: { Authorization: `Bearer ${process.env.API_KEY_YELP}` },
     })
         .then((res) => {
-        console.log(res.data.businesses[0].name);
+        for (const item of res.data.businesses) {
+            console.log(item.name);
+        }
     })
         .catch((err) => {
         console.log(err);
     });
 };
-exports.getRestaurant = getRestaurant;
-getRestaurant("Vancouver, BC");
+exports.getRestaurants = getRestaurants;
+/**
+ * Input:
+ *  filter: string => Categories separated by ,
+ * Output:
+ *  Returns an object containing restaurants from the location specified matching categories from the filter string
+ */
+const getRestaurantsWithFilter = (filter) => {
+    let businesses = [];
+    const location = "Vancouver, BC";
+    return axios_1.default
+        .get(`https://api.yelp.com/v3/businesses/search?location=${location}&categories=restaurants,${filter}`, {
+        headers: { Authorization: `Bearer ${process.env.API_KEY_YELP}` },
+    })
+        .then((res) => {
+        for (const item of res.data.businesses) {
+            businesses.push(item.name);
+        }
+        return businesses;
+    })
+        .catch((err) => {
+        console.log(err);
+    });
+};
+exports.getRestaurantsWithFilter = getRestaurantsWithFilter;
+// Randomize array in place using Durstenfeld Shuffle algorithm
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+};
--- api/externalAPI/yelp.ts
+++ api/externalAPI/yelp.ts
@@ -1,20 +1,59 @@
 import axios from "axios";
-require('dotenv').config();
+require("dotenv").config();
 
-const getRestaurant = (location: string) => {
-  return axios.get(`https://api.yelp.com/v3/businesses/search?location=${location}&categories=restaurants&limit=10`, {
-    headers: { 'Authorization': `Bearer ${process.env.API_KEY_YELP}`}
-  })
-  .then((res: any) => {
-    console.log(res.data.businesses[0].name);
-  })
-  .catch((err: any) => {
-    console.log(err);
-  })
+const getRestaurants = (location: string) => {
+  return axios
+    .get(
+      `https://api.yelp.com/v3/businesses/search?location=${location}&categories=restaurants`,
+      {
+        headers: { Authorization: `Bearer ${process.env.API_KEY_YELP}` },
+      }
+    )
+    .then((res: any) => {
+      for (const item of res.data.businesses) {
+        console.log(item.name);
+      }
+    })
+    .catch((err: any) => {
+      console.log(err);
+    });
+};
+
+/**
+ * Input:
+ *  filter: string => Categories separated by ,
+ * Output:
+ *  Returns an object containing restaurants from the location specified matching categories from the filter string
+ */
+const getRestaurantsWithFilter = (filter: string) => {
+  let businesses: string[] = [];
+  const location: string = "Vancouver, BC";
+  return axios
+    .get(
+      `https://api.yelp.com/v3/businesses/search?location=${location}&categories=restaurants,${filter}`,
+      {
+        headers: { Authorization: `Bearer ${process.env.API_KEY_YELP}` },
+      }
+    )
+    .then((res: any) => {
+      for (const item of res.data.businesses) {
+        businesses.push(item.name);
+      }
+      return businesses;
+    })
+    .catch((err: any) => {
+      console.log(err);
+    });
+};
+
+// Randomize array in place using Durstenfeld Shuffle algorithm
+const shuffleArray = (array: any) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
 }
 
-getRestaurant("Vancouver, BC");
-
-export {
-  getRestaurant
-}
+export { getRestaurants, getRestaurantsWithFilter };
