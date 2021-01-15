97160305c3d00da30f13a3fdefff11934d8ba793
John <jooke98@outlook.com>
2021-01-14 19:21:46 -0700

    Started Partner Management styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -27,18 +27,12 @@ function App() {
           <div className="collapse navbar-collapse" id="navbarResponsive">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="#">Home
+                <a className="nav-link" href="/">Partner Management
               <span className="sr-only">(current)</span>
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Services</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Contact</a>
+                <a className="nav-link" href='#'>Match</a>
               </li>
             </ul>
           </div>
@@ -47,8 +41,21 @@ function App() {
 
       <div className="container">
         <div className="row">
-          <div className="col-lg-12 text-center">
-            <h1 className="mt-5">A Bootstrap 4 Starter Template</h1>
+          <div className="col-lg-12">
+            <div className="current-partner">
+              My Partner
+            </div>
+            <div className="partners">
+              <h1 className="mt-5">Partners</h1>
+              <ul>
+                <li>
+                  Partner 1
+                </li>
+                <li>
+                  Partner 2
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
