30e89d272a8e78f04f4e6339b979064a47e0d372
John <jooke98@outlook.com>
2021-01-14 18:54:31 -0700

    formatted files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -49,11 +49,6 @@ function App() {
         <div className="row">
           <div className="col-lg-12 text-center">
             <h1 className="mt-5">A Bootstrap 4 Starter Template</h1>
-            <p className="lead">Complete with pre-defined file paths and responsive navigation!</p>
-            <ul className="list-unstyled">
-              <li>Bootstrap 4.5.3</li>
-              <li>jQuery 3.5.1</li>
-            </ul>
           </div>
         </div>
       </div>
