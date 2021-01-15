5a7c74b3f500cac8eaeb809d374cd54d03ebf108
John <jooke98@outlook.com>
2021-01-14 17:40:40 -0700

    Added initial page to app. CSS loaded

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './bootstrap/vendor/bootstrap/css/bootstrap.css'
 import axios from "axios";
 
 function App() {
@@ -13,23 +13,48 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          <p> <button onClick={testBackEnd}>Test Backend</button></p>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <body>
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div className="container">
+      <a className="navbar-brand" href="#">Start Bootstrap</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarResponsive">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">Home
+              <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Services</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
     </div>
+  </nav>
+
+  <div className="container">
+    <div className="row">
+      <div className="col-lg-12 text-center">
+        <h1 className="mt-5">A Bootstrap 4 Starter Template</h1>
+        <p className="lead">Complete with pre-defined file paths and responsive navigation!</p>
+        <ul className="list-unstyled">
+          <li>Bootstrap 4.5.3</li>
+          <li>jQuery 3.5.1</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+</body>
   );
 }
 
