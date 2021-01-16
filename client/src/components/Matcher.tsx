c81041de82fc6e425fbc80fdc37ee6e94bf697b3
John <jooke98@outlook.com>
2021-01-15 17:34:10 -0700

    Added star rating custom fill

--- client/src/components/Matcher.tsx
+++ client/src/components/Matcher.tsx
@@ -4,10 +4,22 @@ import '../bootstrap/vendor/bootstrap/css/bootstrap.css'
 
 function Matcher() {
 
+  const topStyle = {
+    width: "105%" //110% is full stars
+  };
+
   return (
     <div className="restaurant">
       <h2>Restaurant Name</h2>
       <img src="https://lunawood.com/wp-content/uploads/2018/02/placeholder-image-300x225.png" />
+      <div className="rating">
+        <div className="top" style={topStyle}>
+          <span>★</span><span>★</span><span>★</span><span>★</span><span>★</span>
+        </div>
+        <div className="bottom">
+          <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+        </div>
+      </div>
     </div>
   )
 }
