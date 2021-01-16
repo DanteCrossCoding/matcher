b8a6230d416ccc5a22d3c14f8d839da0658a6c52
John <jooke98@outlook.com>
2021-01-15 18:01:12 -0700

    adding padding and removed extra styling

--- client/src/components/Partner.tsx
+++ client/src/components/Partner.tsx
@@ -5,7 +5,7 @@ import '../bootstrap/vendor/bootstrap/css/bootstrap.css'
 function Partner(props :any) {
   return (
     <div className="current-partner">
-      <h1 className="mt-5">{props.name}</h1>
+      <h1>{props.name}</h1>
       <img src="https://www.dovercourt.org/wp-content/uploads/2019/11/610-6104451_image-placeholder-png-user-profile-placeholder-image-png-286x300.jpg" width="200px" />
   <h5>{props.email}</h5>
     </div>
