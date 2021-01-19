b95d4b3b903a40f0a663f1abb36af834e4c6a3a7
remy29 <arthur.d.remy@gmail.com>
2021-01-18 19:52:36 -0800

    added more options to drop down list

--- client/src/components/FormContainer.js
+++ client/src/components/FormContainer.js
@@ -17,11 +17,22 @@ export default function FormContainer(props) {
     >
       <Dropdown.Item eventKey="japanese">Japanese</Dropdown.Item>
       <Dropdown.Item eventKey="chinese">Chinese</Dropdown.Item>
+      <Dropdown.Item eventKey="dimsum">Dim sum</Dropdown.Item>
       <Dropdown.Item eventKey="seafood">Seafood</Dropdown.Item>
       <Dropdown.Item eventKey="italian">Italian</Dropdown.Item>
       <Dropdown.Item eventKey="brunch">Brunch</Dropdown.Item>
+      <Dropdown.Item eventKey="french">French</Dropdown.Item>
+      <Dropdown.Item eventKey="korean">Korean</Dropdown.Item>
+      <Dropdown.Item eventKey="sushi">Sushi</Dropdown.Item>
+      <Dropdown.Item eventKey="ramen">Ramen</Dropdown.Item>
       <Dropdown.Item eventKey="vietnamese">Vietnamese</Dropdown.Item>
       <Dropdown.Item eventKey="mexican">Mexican</Dropdown.Item>
+      <Dropdown.Item eventKey="lebanese">Lebanese</Dropdown.Item>
+      <Dropdown.Item eventKey="vegetarian">Vegetarian</Dropdown.Item>
+      <Dropdown.Item eventKey="vegan">Vegan</Dropdown.Item>
+      <Dropdown.Item eventKey="thai">Thai</Dropdown.Item>
+      <Dropdown.Item eventKey="bbq">BBQ</Dropdown.Item>
+      <Dropdown.Item eventKey="greek">Greek</Dropdown.Item>
     </DropdownButton>
   );
 }
