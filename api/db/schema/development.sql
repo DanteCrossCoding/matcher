738a7e6709b7c93ce5efee387c7e8ab67c195dbd
John <jooke98@outlook.com>
2021-01-20 15:24:11 -0700

    Added team to postgres database

--- api/db/schema/development.sql
+++ api/db/schema/development.sql
@@ -1,7 +1,10 @@
 INSERT INTO users (name, email) VALUES
   ('Bob Ross', 'Test@test.com'),
   ('Stanley Park', '4ever@stlyprk.bc.ca'),
-  ('Bart Simpson', 'fart@farts.com');
+  ('Bart Simpson', 'fart@farts.com'),
+  ('Dante Cross', 'dantecross@outlook.com'),
+  ('Arthur Remy', 'remy_29@msn.com'),
+  ('Sean Oyler', 'sean.oyler@gmail.com');
 
 INSERT INTO matches (restaurant, user_id, partner_id) VALUES
   ('604-669-7769', 1, 2),
