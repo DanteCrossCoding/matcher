fbb7905882255d1a4d0e157fdd7b90eed90888e0
John <jooke98@outlook.com>
2021-01-22 13:28:22 -0700

    Cleaned react components

--- client/src/components/MatchListItem.tsx
+++ client/src/components/MatchListItem.tsx
@@ -3,7 +3,6 @@ import '../App.scss';
 import '../bootstrap/vendor/bootstrap/css/bootstrap.css'
 
 function MatchListItem(props: any) {
-  console.log('match list')
   return (
     <div>
       <h1>{props.match.restaurant}</h1>
