c6021ce16078437e26ee006c7392d055a0ee780f
John <jooke98@outlook.com>
2021-01-24 18:28:50 -0700

    Images loaded into database and loading on page

--- api/db/schema/development.sql
+++ api/db/schema/development.sql
@@ -1,19 +1,15 @@
+INSERT INTO users (name, email, avatar) VALUES
+  ('Bob Ross', 'test@test.com', 'https://yt3.ggpht.com/ytc/AAUvwnhkZjfj3AhZNOvbxzIzVLTKZZHGLAlIHVstuYx1=s88-c-k-c0x00ffffff-no-rj'),
+  ('Bart Simpson', 'fart@farts.com', 'https://images.newrepublic.com/5485c82606455ce71f6659fc57975a096a291c11.jpeg'),
+  ('Bob Mango', 'bob@mango.com', 'https://static.wikia.nocookie.net/annoyingorange/images/4/46/MangoLeStache.png/revision/latest?cb=20140720020208'),
+  ('Sue Mango', 'sue@mango.com', 'https://www.seekpng.com/png/full/18-181383_mango-clipart-apple-clip-art-mango-png.png');
+  
 INSERT INTO users (name, email) VALUES
-<<<<<<< HEAD
-  ('Bob Ross', 'Test@test.com'),
-  ('Bob Mango', 'bob@mango.com'),
-  ('Sue Mango', 'sue@mango.com'),
-=======
-  ('Bob Ross', 'test@test.com'),
->>>>>>> f02ae6a79c4aec911184786df8860559a7386fea
   ('Stanley Park', '4ever@stlyprk.bc.ca'),
-  ('Bart Simpson', 'fart@farts.com'),
   ('Dante Cross', 'dantecross@outlook.com'),
   ('Arthur Remy', 'remy_29@msn.com'),
   ('Sean Oyler', 'sean.oyler@gmail.com'),
-  ('John Oke', 'jooke98@outlook.com'),
-  ('Bob Mango', 'bob@mango.com'),
-  ('Sue Mango', 'sue@mango.com');
+  ('John Oke', 'jooke98@outlook.com');
 
 INSERT INTO matches (restaurant, user_id, partner_id) VALUES
   ('Dinesty Dumpling House', 1, 2),
